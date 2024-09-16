a1e30e6c0e639ba826157141970cfb4e1a4adc1f
omar0141 <75172714+omar0141@users.noreply.github.com>
2024-09-16 09:52:44 +0000

    Update om_console.dart

--- lib/om_console.dart
+++ lib/om_console.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class Console {
   static void log(
@@ -12,10 +13,14 @@ class Console {
       try {
         int lastId =
             (OmConsole.orgLogs.isEmpty ? 0 : OmConsole.orgLogs.last.id) + 1;
-        OmConsole.logs.value
-            .add(Log("$message", type: type, textColor: textColor, id: lastId));
-        OmConsole.orgLogs
-            .add(Log("$message", type: type, textColor: textColor, id: lastId));
+        Log logData = Log(
+          "$message",
+          type: type,
+          textColor: textColor,
+          id: lastId,
+        );
+        OmConsole.logs.value.add(logData);
+        OmConsole.orgLogs.add(logData);
         OmConsole.filterWithTags(OmConsole.searchConroller.text);
         // ignore: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
         OmConsole.logs.notifyListeners();
@@ -45,15 +50,16 @@ class Console {
           "$url $method $headers $body $statusCode $response",
           url: url,
           method: method,
-          headers: JsonEncoder.withIndent('  ').convert(headers),
-          body: JsonEncoder.withIndent('  ').convert(body),
+          headers: const JsonEncoder.withIndent('  ').convert(headers),
+          body: const JsonEncoder.withIndent('  ').convert(body),
           statusCode: statusCode,
           response: prettyResponse,
           textColor: textColor,
           backgroundColor: backgroundColor,
           id: lastId,
           type: LogType.http,
-          curlCommand: OmConsole.generateCurlCommand(url, body),
+          curlCommand:
+              OmConsole.generateCurlCommandWithJson(url, headers, body),
         );
         OmConsole.logs.value.add(logHttp);
         OmConsole.orgLogs.add(logHttp);
@@ -64,100 +70,6 @@ class Console {
         print(e.toString());
       }
     });
-  }
-}
-
-class OmConsole {
-  static final List<Log> orgLogs = [];
-  static final ValueNotifier<List<Log>> logs = ValueNotifier<List<Log>>([]);
-  static List<LogType?> logTypes = [];
-  static TextEditingController searchConroller = TextEditingController();
-
-  static void clear() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      try {
-        for (var log in logs.value) {
-          orgLogs.removeWhere((e) => e.id == log.id);
-        }
-        logs.value.clear();
-
-        // ignore: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
-        logs.notifyListeners();
-      } catch (e) {
-        print(e.toString());
-      }
-    });
-  }
-
-  static void search(String text) {
-    // WidgetsBinding.instance.addPostFrameCallback((_) {
-    try {
-      if (text.isEmpty) {
-        logs.value = orgLogs;
-      } else {
-        logs.value = orgLogs
-            .where(
-                (log) => log.message.toLowerCase().contains(text.toLowerCase()))
-            .toList();
-      }
-      // ignore: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
-      logs.notifyListeners();
-    } catch (e) {
-      print(e.toString());
-    }
-    // });
-  }
-
-  static String generateCurlCommand(String url, Map<String, dynamic> formData) {
-    String curlCommand = "curl --location '$url' \\\n";
-
-    formData.forEach((key, value) {
-      curlCommand += "--form '$key=\"$value\"' \\\n";
-    });
-
-    // Remove the trailing backslash and newline
-    curlCommand = curlCommand.trimRight().replaceAll(RegExp(r'\\\n$'), '');
-
-    return curlCommand;
-  }
-
-  static void filterWithTags(String text) {
-    try {
-      if (logTypes.isEmpty || logTypes.contains(null)) {
-        if (text.isEmpty) {
-          // logs.value.clear();
-          logs.value = orgLogs
-              .map((e) => Log(e.message,
-                  url: e.url,
-                  method: e.method,
-                  headers: e.headers,
-                  body: e.body,
-                  statusCode: e.statusCode,
-                  response: e.response,
-                  textColor: e.textColor,
-                  id: e.id,
-                  type: e.type,
-                  curlCommand: e.curlCommand,
-                  backgroundColor: e.backgroundColor))
-              .toList();
-        } else {
-          logs.value = orgLogs
-              .where((log) =>
-                  log.message.toLowerCase().contains(text.toLowerCase()))
-              .toList();
-        }
-      } else {
-        logs.value = orgLogs
-            .where((log) =>
-                logTypes.contains(log.type) &&
-                log.message.toLowerCase().contains(text.toLowerCase()))
-            .toList();
-      }
-      // ignore: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
-      logs.notifyListeners();
-    } catch (e) {
-      print(e.toString());
-    }
   }
 
   static void consoleLisitener(function) {
@@ -180,6 +92,146 @@ class OmConsole {
         },
       ),
     );
+  }
+}
+
+class OmConsole {
+  static final List<Log> orgLogs = [];
+  static final ValueNotifier<List<Log>> logs = ValueNotifier<List<Log>>([]);
+  static List<LogType?> logTypes = [];
+  static TextEditingController searchConroller = TextEditingController();
+  static int currentSearchScrollIndex = 0;
+  static Log? currentSearch;
+  static final ItemScrollController itemScrollController =
+      ItemScrollController();
+
+  static void clear() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      try {
+        for (var log in logs.value) {
+          orgLogs.removeWhere((e) => e.id == log.id);
+        }
+        logs.value.clear();
+
+        // ignore: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
+        logs.notifyListeners();
+      } catch (e) {
+        print(e.toString());
+      }
+    });
+  }
+
+  static void searchPaging({
+    bool forward = false,
+    bool back = false,
+  }) {
+    try {
+      String text = searchConroller.text;
+      List<Log> searchLogs = [];
+      if (text.isEmpty) {
+        searchLogs = [];
+      } else {
+        searchLogs = logs.value
+            .where(
+                (log) => log.message.toLowerCase().contains(text.toLowerCase()))
+            .toList();
+      }
+      if (searchLogs.isEmpty) {
+        currentSearch = null;
+        currentSearchScrollIndex = 0;
+        return;
+      }
+      if (forward) {
+        back = false;
+        currentSearchScrollIndex += 1;
+        if (currentSearchScrollIndex > searchLogs.length - 1) {
+          currentSearchScrollIndex = searchLogs.length - 1;
+        }
+        currentSearch = searchLogs[currentSearchScrollIndex];
+        int index = logs.value.indexWhere((e) => currentSearch!.id == e.id);
+        if (index < 0) {
+          itemScrollController.scrollTo(
+              index: 0,
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeInOutCubic);
+        } else {
+          itemScrollController.scrollTo(
+              index: index,
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeInOutCubic);
+        }
+      } else {
+        forward = false;
+        currentSearchScrollIndex -= 1;
+        if (currentSearchScrollIndex < 0) currentSearchScrollIndex = 0;
+        currentSearch = searchLogs[currentSearchScrollIndex];
+        int index = logs.value.indexWhere((e) => currentSearch!.id == e.id);
+        if (index < 0) {
+          itemScrollController.scrollTo(
+              index: 0,
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeInOutCubic);
+        } else {
+          itemScrollController.scrollTo(
+              index: index,
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeInOutCubic);
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  static String generateCurlCommandWithJson(
+      String url, Map<String, dynamic> headers, Map<String, dynamic> jsonData) {
+    // Start building the curl command
+    String curlCommand = "curl --location '$url' \\\n";
+
+    // Add headers to the command
+    headers.forEach((key, value) {
+      curlCommand += "--header '$key: $value' \\\n";
+    });
+
+    // Convert the Map to a pretty-printed JSON string
+    String jsonString = jsonEncode(jsonData);
+
+    // Add the data payload to the command
+    curlCommand += "--data '$jsonString'";
+
+    return curlCommand;
+  }
+
+  static void filterWithTags(String text) {
+    try {
+      currentSearchScrollIndex = 0;
+      if (logTypes.isEmpty || logTypes.contains(null)) {
+        // logs.value.clear();
+        logs.value = orgLogs
+            .map((e) => Log(
+                  e.message,
+                  url: e.url,
+                  method: e.method,
+                  headers: e.headers,
+                  body: e.body,
+                  statusCode: e.statusCode,
+                  response: e.response,
+                  textColor: e.textColor,
+                  id: e.id,
+                  type: e.type,
+                  curlCommand: e.curlCommand,
+                  backgroundColor: e.backgroundColor,
+                ))
+            .toList();
+      } else {
+        logs.value =
+            orgLogs.where((log) => logTypes.contains(log.type)).toList();
+      }
+      // ignore: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
+      logs.notifyListeners();
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
 
